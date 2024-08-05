8aec10cc769c428c6975783094e51da74dea3dac
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2024-08-05 20:47:06 +0530

    Update ForegroundService.java
    keepAwake() commented

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -127,44 +127,44 @@ public class ForegroundService extends Service {
      * by the OS.
      */
     @SuppressLint("WakelockTimeout")
-    private void keepAwake()
-    {
-        JSONObject settings = BackgroundMode.getSettings();
+    // private void keepAwake()
+    // {
+    //     JSONObject settings = BackgroundMode.getSettings();
  
-        boolean isSilent    = settings.optBoolean("silent", false);
+    //     boolean isSilent    = settings.optBoolean("silent", false);
 
-        if (!isSilent) {
-          startForeground(NOTIFICATION_ID, makeNotification(),ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION);
+    //     if (!isSilent) {
+    //       startForeground(NOTIFICATION_ID, makeNotification(),ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION);
+    //     }
+
+    //     PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
+
+    //     wakeLock = pm.newWakeLock(
+    //             PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
+
+    //     wakeLock.acquire();
+    // }
+
+ private void keepAwake() {
+    JSONObject settings = BackgroundMode.getSettings();
+    boolean isSilent = settings.optBoolean("silent", false);
+
+    if (!isSilent) {
+        Notification notification = makeNotification();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+           startForeground(NOTIFICATION_ID, notification,FOREGROUND_SERVICE_TYPE_MICROPHONE);
+        } else {
+           startForeground(NOTIFICATION_ID, notification);
         }
-
-        PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
-
-        wakeLock = pm.newWakeLock(
-                PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
-
-        wakeLock.acquire();
     }
 
-//  private void keepAwake() {
-//     JSONObject settings = BackgroundMode.getSettings();
-//     boolean isSilent = settings.optBoolean("silent", false);
+    PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
 
-//     if (!isSilent) {
-//         Notification notification = makeNotification();
-//         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//            startForeground(NOTIFICATION_ID, notification,FOREGROUND_SERVICE_TYPE_MICROPHONE);
-//         } else {
-//            startForeground(NOTIFICATION_ID, notification);
-//         }
-//     }
+    wakeLock = pm.newWakeLock(
+            PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
 
-//     PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
-
-//     wakeLock = pm.newWakeLock(
-//             PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
-
-//     wakeLock.acquire();
-// }
+    wakeLock.acquire();
+}
 
 
 
