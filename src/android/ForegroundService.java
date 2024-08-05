9b76fd6d4422a44a136cf47cf3df0952cafcfc5f
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2024-08-05 19:22:10 +0530

    Update ForegroundService.java
    ServiceCompat.FOREGROUND_SERVICE_TYPE_MICROPHONE update

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -133,7 +133,7 @@ public class ForegroundService extends Service {
         boolean isSilent    = settings.optBoolean("silent", false);
 
         if (!isSilent) {
-            startForeground(NOTIFICATION_ID, makeNotification());
+            startForeground(NOTIFICATION_ID, makeNotification(),ServiceCompat.FOREGROUND_SERVICE_TYPE_MICROPHONE);
         }
 
         PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
