04476ff83a819ba8b51c9e703d0b28315bf0083a
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2024-08-07 02:32:29 +0530

    Update ForegroundService.java
    START_REDELIVER_INTENT

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -118,7 +118,8 @@ public class ForegroundService extends Service {
      */
     @Override
     public int onStartCommand (Intent intent, int flags, int startId) {
-        return START_STICKY;
+        // return START_STICKY;
+           return START_REDELIVER_INTENT;
     }
 
     /**
