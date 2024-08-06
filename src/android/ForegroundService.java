a167f9e0f1d9d45b4d6c5dbc50736baab4426822
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2024-08-06 14:46:43 +0530

    Update ForegroundService.java
    update

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -21,7 +21,7 @@
 
 package de.appplant.cordova.plugin.background;
 
-import androidx.core.app.ServiceCompat;
+// import androidx.core.app.ServiceCompat;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Notification;
@@ -36,7 +36,7 @@ import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.app.NotificationChannel;
-import android.content.pm.ServiceInfo;
+// import android.content.pm.ServiceInfo;
 
 
 import org.json.JSONObject;
@@ -126,44 +126,44 @@ public class ForegroundService extends Service {
      * by the OS.
      */
     @SuppressLint("WakelockTimeout")
-    // private void keepAwake()
-    // {
-    //     JSONObject settings = BackgroundMode.getSettings();
+   private void keepAwake()
+     {
+         JSONObject settings = BackgroundMode.getSettings();
  
-    //     boolean isSilent    = settings.optBoolean("silent", false);
+        boolean isSilent    = settings.optBoolean("silent", false);
 
-    //     if (!isSilent) {
-    //       startForeground(NOTIFICATION_ID, makeNotification(),ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION);
-    //     }
-
-    //     PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
-
-    //     wakeLock = pm.newWakeLock(
-    //             PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
-
-    //     wakeLock.acquire();
-    // }
-
- private void keepAwake() {
-    JSONObject settings = BackgroundMode.getSettings();
-    boolean isSilent = settings.optBoolean("silent", false);
-
-    if (!isSilent) {
-        Notification notification = makeNotification();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-           startForeground(NOTIFICATION_ID, notification,FOREGROUND_SERVICE_TYPE_MICROPHONE);
-        } else {
-           startForeground(NOTIFICATION_ID, notification);
+         if (!isSilent) {
+          startForeground(NOTIFICATION_ID, makeNotification());
         }
+
+        PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
+
+        wakeLock = pm.newWakeLock(
+                PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
+
+        wakeLock.acquire();
     }
 
-    PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+//  private void keepAwake() {
+//     JSONObject settings = BackgroundMode.getSettings();
+//     boolean isSilent = settings.optBoolean("silent", false);
 
-    wakeLock = pm.newWakeLock(
-            PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
+//     if (!isSilent) {
+//         Notification notification = makeNotification();
+//         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            startForeground(NOTIFICATION_ID, notification,FOREGROUND_SERVICE_TYPE_MICROPHONE);
+//         } else {
+//            startForeground(NOTIFICATION_ID, notification);
+//         }
+//     }
 
-    wakeLock.acquire();
-}
+//     PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+
+//     wakeLock = pm.newWakeLock(
+//             PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
+
+//     wakeLock.acquire();
+// }
 
 
 
